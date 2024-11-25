4f6fcf2cfedd0a3214f9043ad7caa7bf983bcb72
agostinacorcuera <agostinacorcuera@hotmail.com>
2024-11-25 16:03:02 -0300

    Initial Commit

---
+++ CardsGame/CardsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameApp.swift
+//  CardsGame
+//
+//  Created by Agostina Corcuera on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardsGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardsGame
+//
+//  Created by Agostina Corcuera on 25/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
